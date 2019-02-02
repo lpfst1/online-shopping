e973b383057fb55f358e4e8173dc8bc9f0f488d9
Louie Francioni <Louie.Francioni@gmail.com>
2019-02-01 19:37:54 -0500

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/knet/kzn/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package knet.kzn.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
